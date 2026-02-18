4fdef84b9b481bf5d225ef66afe75bd14e16492e
Rares <raresvacareanu@gmail.com>
2026-02-19 00:59:48 +0100

    Repaired flake8 styling errors and added automatic hyperparameter tuning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
-import re
-from preprocessing_normalisation import preprocess,normalise
+from preprocessing_normalisation import preprocess, normalise
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,9 +7,11 @@ from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import (
     accuracy_score,
+    log_loss,
     f1_score,
     confusion_matrix,
     classification_report,
+    hinge_loss,
     ConfusionMatrixDisplay,
 )
 
@@ -19,106 +20,151 @@ import pandas as pd
 
 
 SEED = 69
-
-# download the AG news dataset
-dataset_dict = load_dataset("SetFit/ag_news")
-"""
-Dataset downloads the test and train split separately
-It save them in a dictionary where the first element
-is the train dataset, and the second one is the test dataset
-
-they are both Dataset objects containing:
-    features: text label label_text
-    num_rows: int
-"""
-# Preprocessing
-train_full = dataset_dict["train"].to_pandas()
-test = dataset_dict["test"].to_pandas()
-
-train_full["text"] = train_full["text"].str.lower()
-test["text"] = test["text"].str.lower()
-
-#applying our preprocessing and normalisation functions from the other file
-train_full["text"] = train_full["text"].apply(preprocess)
-train_full["text"] = train_full["text"].apply(normalise)
-print(train_full.head(20))
+LABELS = {0: "World", 1: "Sports", 2: "Business", 3: "Sci/Tech"}
 
 
-# split the training dataset into train and validation
-train, dev = train_test_split(
-    train_full, test_size=0.16, random_state=SEED, stratify=train_full["label"]
-)
+def calculate_metrics(real: pd.DataFrame, pred: list,  model: str) -> None:
+    print(f"\n{model} metrics:\n")
+    print(f"Accuracy: {accuracy_score(real['label'], pred):.3f}")
+    print(f"F1-Score: {f1_score(real['label'], pred, average='macro'):.3f}")
+    print(f"Confusion Matrix: \n{confusion_matrix(real['label'], pred)}\n")
 
-print(f"Train size: {len(train)} rows")
-print(f"Dev size:   {len(dev)} rows")
-print(f"Test size:  {len(test)} rows")
+    cm = confusion_matrix(real['label'], pred)
+    disp = ConfusionMatrixDisplay(
+        confusion_matrix=cm, display_labels=real['label_text'].unique()
+    )
+    disp.plot(xticks_rotation="vertical")
+    plt.title(f"Confusion Matrix: {model}")
+    plt.show()
 
-# this is the TF-IDF function
-#! the hyperparameters might need to be changed
-tf_idf = TfidfVectorizer(
-    lowercase=True, stop_words="english", max_features=10000, ngram_range=(1, 2)
-)
+    predictions = pd.DataFrame({
+        "text": real["text"],
+        "true_label": real["label"].map(LABELS),
+        "pred_label": pd.Series(pred).map(LABELS),
+    })
 
-# apply TF-IDF on the documents
-X_train = tf_idf.fit_transform(train["text"])
-# do not fit on the dev and an test as it results in leakage
-X_dev = tf_idf.transform(dev["text"])
-X_test = tf_idf.transform(test["text"])
+    errors = predictions[
+        predictions["true_label"] != predictions["pred_label"]
+    ]
 
-# print(X_train)
+    print(f"\nTotal Errors: {len(errors)}")
+    print("Displaying first 20 misclassifications:\n")
 
-lr = LogisticRegression(random_state=SEED)
-
-svm = LinearSVC(loss="squared_hinge", random_state=SEED)
-
-lr.fit(X_train, train["label"])
-svm.fit(X_train, train["label"])
-
-lr_pred = lr.predict(X_test)
-svm_pred = svm.predict(X_test)
-
-# Is this not Logistic Regression?
-print("Linear Regression metrics:\n")
-print(f"Accuracy: {accuracy_score(test['label'], lr_pred):.3f}")
-print(f"F1-Score: {f1_score(test['label'], lr_pred, average='macro'):.3f}")
-print(f"Confusion Matrix: \n{confusion_matrix(test['label'], lr_pred)}\n")
-
-cm = confusion_matrix(test["label"], lr_pred)
-disp = ConfusionMatrixDisplay(
-    confusion_matrix=cm, display_labels=test["label_text"].unique()
-)
-disp.plot(xticks_rotation="vertical")
-plt.title("Confusion Matrix: Linear Regression")
-plt.show()
+    for i, doc in errors.head(20).iterrows():
+        print(f"Article number {i}:")
+        print(f"TRUE: {doc['true_label']} | PRED: {doc['pred_label']}")
+        print(f"TEXT: {doc['text']}")
+        print("-" * 80)
 
 
-# And this being Linear Regression?
-print("SVM metrics:\n")
-print(f"Accuracy: {accuracy_score(test['label'], svm_pred):.3f}")
-print(f"F1-Score: {f1_score(test['label'], svm_pred, average='macro'):.3f}")
-print(f"Confusion Matrix: \n{confusion_matrix(test['label'], svm_pred)}\n")
-cm = confusion_matrix(test["label"], svm_pred)
-disp = ConfusionMatrixDisplay(
-    confusion_matrix=cm, display_labels=test["label_text"].unique()
-)
-disp.plot(xticks_rotation="vertical")
-plt.title("Confusion Matrix: SVM")
-plt.show()
+def hyperparameter_tuning(
+        train: pd.DataFrame,
+        dev: pd.DataFrame,
+        model: LogisticRegression | LinearSVC,
+        epochs: int = 15,
+        ) -> TfidfVectorizer:
+
+    max_features = 1000
+    picked_tf_idf = None
+    best = float('inf')
+
+    for i in range(epochs):
+        print(f"\nEpoch {i}:")
+
+        tf_idf = TfidfVectorizer(
+            lowercase=True,
+            stop_words="english",
+            max_features=max_features,
+            ngram_range=(1, 2)
+        )
+
+        # apply TF-IDF on the documents
+        X_train = tf_idf.fit_transform(train["text"])
+        X_dev = tf_idf.transform(dev["text"])
+
+        # train the model
+        model.fit(X_train, train["label"])
+
+        if isinstance(model, LogisticRegression):
+            train_pred = model.predict_proba(X_train)
+            dev_pred = model.predict_proba(X_dev)
+            # calculate the log loss
+            test_loss = log_loss(train["label"], train_pred)
+            dev_loss = log_loss(dev["label"], dev_pred)
+
+        else:
+            train_pred = model.decision_function(X_train)
+            dev_pred = model.decision_function(X_dev)
+            # calculate the squared hinge loss
+            test_loss = hinge_loss(train["label"], train_pred)**2
+            dev_loss = hinge_loss(dev["label"], dev_pred)**2
+
+        print(f"Test Loss: {test_loss} | Dev Loss: {dev_loss}")
+        # save the TF-IDF that leads to the smallest loss
+        if dev_loss < best:
+            best, picked_tf_idf = dev_loss, tf_idf
+
+        max_features += 2000
+
+    return picked_tf_idf
 
 
-# TODO: Collect the misclassified examples and categorize them
-# TODO: Get rid of code duplication by putting the metrics in a function
+def main() -> None:
 
-lr_df_predictions = pd.DataFrame(
-    {"text": test["text"], "true_label": test["label"], "pred_label": lr_pred}
-)
+    # download the AG news dataset
+    dataset_dict = load_dataset("SetFit/ag_news")
+    """
+    Dataset downloads the test and train split separately
+    It save them in a dictionary where the first element
+    is the train dataset, and the second one is the test dataset
 
-errors = lr_df_predictions[
-    lr_df_predictions["true_label"] != lr_df_predictions["pred_label"]
-]
+    they are both Dataset objects containing:
+        features: text label label_text
+        num_rows: int
+    """
+    # Preprocessing
+    train_full = dataset_dict["train"].to_pandas()
+    test = dataset_dict["test"].to_pandas()
+    train_full["text"] = train_full["text"].apply(preprocess).apply(normalise)
+    test["text"] = test["text"].apply(preprocess).apply(normalise)
 
-print(f"Total Errors: {len(errors)}")
-print("Displaying first 20 misclassifications:")
+    print(train_full.head(5))
 
-# pd.set_option('display.max_colwidth', 150)
-display(errors.head(20))
+    # split the training dataset into train and validation
+    train, dev = train_test_split(
+        train_full,
+        test_size=0.16,
+        random_state=SEED,
+        stratify=train_full["label"]
+    )
+
+    print(f"\nTrain size: {len(train)} rows")
+    print(f"Dev size:   {len(dev)} rows")
+    print(f"Test size:  {len(test)} rows")
+
+    lr = LogisticRegression(random_state=SEED, solver="saga")
+    #svm = LinearSVC(loss="squared_hinge", random_state=SEED)
+
+    tf_idf = hyperparameter_tuning(
+        train=train,
+        dev=dev,
+        model=lr
+    )
+
+    # apply TF-IDF on the documents
+    X_train = tf_idf.fit_transform(train["text"])
+    # do not fit on the dev and on test as it results in leakage
+    X_test = tf_idf.transform(test["text"])
+
+    lr.fit(X_train, train["label"])
+    lr_pred = lr.predict(X_test)
+    calculate_metrics(test, lr_pred, "Logistic Regression")
+
+
+    #svm.fit(X_train, train["label"])
+    #svm_pred = svm.predict(X_test)
+    #calculate_metrics(test, svm_pred, "SVM")
+
+
+if __name__ == "__main__":
+    main()
--- preprocessing_normalisation.py
+++ preprocessing_normalisation.py
@@ -4,7 +4,7 @@ import pandas as pd
 from nltk.stem import LancasterStemmer
 
 
-def preprocess(text:str)->str:
+def preprocess(text: str) -> str:
 
     def replace_contractions(text):
         """Replace contractions in string of text"""
@@ -13,30 +13,33 @@ def preprocess(text:str)->str:
     def remove_URL(sample):
         """Remove URLs from a sample string"""
         return re.sub(r"http\S+", "", sample)
-    
+
     text = remove_URL(text)
     text = replace_contractions(text)
 
     return text
 
-def normalise(text:str)->str:
+
+def normalise(text: str) -> str:
 
     lancaster = LancasterStemmer()
 
     def lowerise(text):
         return text.lower()
-    
+
     def remove_punctuation(text):
         # check if text is None or non string
         if pd.isna(text):
             return text
         return re.sub(r'[^\w\s]', '', str(text))
-    
+
     def remove_common_words(text):
-        common_words = ["the","our","and","a"]
+        common_words = ["the", "our", "and", "a"]
         if pd.isna(text):
             return text
-        text = " ".join([word for word in text.split() if word not in common_words])
+        text = " ".join([
+            word for word in text.split() if word not in common_words
+        ])
         return text
 
     def stem_words(text):
@@ -55,7 +58,6 @@ def normalise(text:str)->str:
 
     #TODO: Can add mnore functions here if needed, can do numbers->words
 
-    
     text = lowerise(text)
     text = remove_punctuation(text)
     text = remove_common_words(text)
