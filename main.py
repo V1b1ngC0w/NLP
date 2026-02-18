af1739a42731d0df43ca381b2550af1868e440a9
jgoversier <j.g.oversier@student.rug.nl>
2026-02-18 14:37:59 +0100

    Added Confusion matrix plot, Added Basic Error Analysis, Added Lowercase Preprocessing Normalization

--- main.py
+++ main.py
@@ -8,80 +8,110 @@ from sklearn.metrics import (
     accuracy_score,
     f1_score,
     confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay,
 )
+
+import matplotlib.pyplot as plt
+import pandas as pd
 
 SEED = 69
 
+# download the AG news dataset
+dataset_dict = load_dataset("SetFit/ag_news")
+"""
+Dataset downloads the test and train split separately
+It save them in a dictionary where the first element
+is the train dataset, and the second one is the test dataset
 
-def main() -> None:
-    # download the AG news dataset
-    dataset_dict = load_dataset("SetFit/ag_news")
-    """
-    Dataset downloads the test and train split separately
-    It save them in a dictionary where the first element
-    is the train dataset, and the second one is the test dataset
+they are both Dataset objects containing:
+    features: text label label_text
+    num_rows: int
+"""
+# Preprocessing
+train_full = dataset_dict["train"].to_pandas()
+test = dataset_dict["test"].to_pandas()
 
-    they are both Dataset objects containing:
-        features: text label label_text
-        num_rows: int
-    """
-    train_full = dataset_dict["train"].to_pandas()
-    test = dataset_dict["test"].to_pandas()
-    # split the training dataset into train and validation
-    train, dev = train_test_split(
-        train_full,
-        test_size=0.16,
-        random_state=SEED,
-        stratify=train_full["label"]
-    )
+train_full["text"] = train_full["text"].str.lower()
+test["text"] = test["text"].str.lower()
+print(train_full.head(20))
 
-    print(f"Train size: {len(train)} rows")
-    print(f"Dev size:   {len(dev)} rows")
-    print(f"Test size:  {len(test)} rows")
 
-    # this is the TF-IDF function
-    #! the hyperparameters might need to be changed
-    tf_idf = TfidfVectorizer(
-        lowercase=True,
-        stop_words="english",
-        max_features=10000,
-        ngram_range=(1, 2)
-    )
+# split the training dataset into train and validation
+train, dev = train_test_split(
+    train_full, test_size=0.16, random_state=SEED, stratify=train_full["label"]
+)
 
-    # apply TF-IDF on the documents
-    X_train = tf_idf.fit_transform(train["text"])
-    # do not fit on the dev and an test as it results in leakage
-    X_dev = tf_idf.transform(dev["text"])
-    X_test = tf_idf.transform(test["text"])
+print(f"Train size: {len(train)} rows")
+print(f"Dev size:   {len(dev)} rows")
+print(f"Test size:  {len(test)} rows")
 
-    #TODO: Implement preprocessing and tokenization
-    #TODO: Find a use for X_dev (might need to be used with gradient descent)
+# this is the TF-IDF function
+#! the hyperparameters might need to be changed
+tf_idf = TfidfVectorizer(
+    lowercase=True, stop_words="english", max_features=10000, ngram_range=(1, 2)
+)
 
-    lr = LogisticRegression(random_state=SEED)
+# apply TF-IDF on the documents
+X_train = tf_idf.fit_transform(train["text"])
+# do not fit on the dev and an test as it results in leakage
+X_dev = tf_idf.transform(dev["text"])
+X_test = tf_idf.transform(test["text"])
 
-    svm = LinearSVC(
-        loss="squared_hinge",
-        random_state=SEED
-    )
+# print(X_train)
 
-    lr.fit(X_train, train["label"])
-    svm.fit(X_train, train["label"])
+lr = LogisticRegression(random_state=SEED)
 
-    lr_pred = lr.predict(X_test)
-    svm_pred = svm.predict(X_test)
+svm = LinearSVC(loss="squared_hinge", random_state=SEED)
 
-    print("Linear Regression metrics:\n")
-    print(f"Accuracy: {accuracy_score(test['label'], lr_pred):.3f}")
-    print(f"F1-Score: {f1_score(test['label'], lr_pred, average='macro'):.3f}")
-    print(f"Confusion Matrix: \n{confusion_matrix(test['label'], lr_pred)}\n")
+lr.fit(X_train, train["label"])
+svm.fit(X_train, train["label"])
 
-    print("SVM metrics:\n")
-    print(f"Accuracy: {accuracy_score(test['label'], svm_pred):.3f}")
-    print(f"F1-Score: {f1_score(test['label'], svm_pred, average='macro'):.3f}")
-    print(f"Confusion Matrix: \n{confusion_matrix(test['label'], svm_pred)}\n")
+lr_pred = lr.predict(X_test)
+svm_pred = svm.predict(X_test)
 
-    #TODO: Collect the misclassified examples and categorize them
-    #TODO: Get rid of code duplication by putting the metrics in a function
+# Is this not Logistic Regression?
+print("Linear Regression metrics:\n")
+print(f"Accuracy: {accuracy_score(test['label'], lr_pred):.3f}")
+print(f"F1-Score: {f1_score(test['label'], lr_pred, average='macro'):.3f}")
+print(f"Confusion Matrix: \n{confusion_matrix(test['label'], lr_pred)}\n")
 
-if __name__ == "__main__":
-    main()
+cm = confusion_matrix(test["label"], lr_pred)
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm, display_labels=test["label_text"].unique()
+)
+disp.plot(xticks_rotation="vertical")
+plt.title("Confusion Matrix: Linear Regression")
+plt.show()
+
+
+# And this being Linear Regression?
+print("SVM metrics:\n")
+print(f"Accuracy: {accuracy_score(test['label'], svm_pred):.3f}")
+print(f"F1-Score: {f1_score(test['label'], svm_pred, average='macro'):.3f}")
+print(f"Confusion Matrix: \n{confusion_matrix(test['label'], svm_pred)}\n")
+cm = confusion_matrix(test["label"], svm_pred)
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm, display_labels=test["label_text"].unique()
+)
+disp.plot(xticks_rotation="vertical")
+plt.title("Confusion Matrix: SVM")
+plt.show()
+
+
+# TODO: Collect the misclassified examples and categorize them
+# TODO: Get rid of code duplication by putting the metrics in a function
+
+lr_df_predictions = pd.DataFrame(
+    {"text": test["text"], "true_label": test["label"], "pred_label": lr_pred}
+)
+
+errors = lr_df_predictions[
+    lr_df_predictions["true_label"] != lr_df_predictions["pred_label"]
+]
+
+print(f"Total Errors: {len(errors)}")
+print("Displaying first 20 misclassifications:")
+
+# pd.set_option('display.max_colwidth', 150)
+display(errors.head(20))
